20c91b0fd60bea92c550c3a86bdd6537c37c04ef
bobita1111 <mr.chitaitinh@gmail.com>
2016-06-15 21:08:30 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/> DUNG CO COPY NHEN: <a href='http://www.tranchitinh.com/'>Tình Trần"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/> Đừng COPY Nhen -Kỳ quá ah!^@@^: <a href='http://www.tranchitinh.com/'>Tình Trần"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';

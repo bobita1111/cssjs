feb477fc57c3c7b847fbcf1a545f3452c055ce1c
bobita1111 <mr.chitaitinh@gmail.com>
2016-09-07 16:24:46 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -1,5 +1,4 @@
-//please contact me giaiphapthuonghieu.org@gmail.com
-//website http://giaiphapthuonghieu.vn or http://faceseo.vn
+
 
 (function($) {
     $.fn.linhnguyen = function(options) {        
@@ -117,7 +116,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://aee.edu.vn/tuyen-sinh/thong-bao-tuyen-sinh-dai-hoc-nam-2016-65.html' target='_blank'><img src='http://3.bp.blogspot.com/-HvdC3Tg7nus/VlPMZ7ICU8I/AAAAAAAABIs/BLJ_OGt1ixs/s1600-r/banner%2Btuyen%2Bsinh430x300.jpg' width='500px'/>Thông tin tuyển sinh Đại học 2016</a>. Đầu vào DỄ - nhận bằng NHANH. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://e-learningvn.net/khoa-hoc-dao-tao-seo-chuyen-sau-tai-tp-ho-chi-minh/' target='_blank'><img src='http://3.bp.blogspot.com/-HvdC3Tg7nus/VlPMZ7ICU8I/AAAAAAAABIs/BLJ_OGt1ixs/s1600-r/banner%2Btuyen%2Bsinh430x300.jpg' width='500px'/>Thông tin tuyển sinh Đại học 2016</a>. Đầu vào DỄ - nhận bằng NHANH. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
